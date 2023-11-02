22e867890f39820ba92d986be47366fe50303059
Azam Amonov <azamamonov555@gmail.com>
2023-11-02 18:25:48 +0500

    Changes on N60

---
+++ N60.FileExplorer.Api/Common/MapperProfiles/DirectoryProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Training.FileExplorer.Api.Models.Dtos;
+using Training.FileExplorer.Application.FileStorage.Models.Storage;
+
+namespace N60.FileExplorer.Api.Common.MapperProfiles;
+
+public class DirectoryProfile : Profile
+{
+    public DirectoryProfile()
+    {
+        CreateMap<StorageDirectory, StorageDirectoryDto>();
+        CreateMap<StorageDirectoryDto, StorageDirectory>();
+    }
+}
---
+++ N60.FileExplorer.Api/Program.cs
@@ -0,0 +1,8 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+await builder.ConfigureAsync();
+var app = builder.Build();
+
+await app.ConfigureAsync();
+await app.RunAsync();
